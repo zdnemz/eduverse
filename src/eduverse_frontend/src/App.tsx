a43613aecc8e747c5e9902ca71ea5dea3bd451ef
zdnemz <zidanemz69@gmail.com>
2025-07-19 18:26:30 +0800

    feat: add React Router DOM and modified vite config

--- src/eduverse_frontend/src/App.tsx
+++ src/eduverse_frontend/src/App.tsx
@@ -1,32 +1,14 @@
-import { useState } from 'react';
-import { eduverse_backend } from '../../declarations/eduverse_backend';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from 'pages/home/Index';
 
-function App() {
-  const [greeting, setGreeting] = useState('');
-  const [name, setName] = useState('');
-
-  function handleClick() {
-    eduverse_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-  }
-
+export default function App() {
   return (
-    <main className="flex h-screen w-full flex-col items-center justify-center bg-gray-900 px-24 text-white">
-      <div className="flex w-full max-w-md gap-2">
-        <input
-          className="w-full border-b px-2 py-1 outline-none focus:outline-none"
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-        />
-        <button className="btn btn-primary" onClick={handleClick}>
-          Click Me!
-        </button>
-      </div>
-
-      <section id="greeting">{greeting}</section>
-    </main>
+    <Router>
+      <Routes>
+        {/* Root Routes */}
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
