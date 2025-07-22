fc33eaf27be5e5e860aa2c2583c7c5e5c5528d0d
zdnemz <zidanemz69@gmail.com>
2025-07-22 14:07:08 +0800

    feat: add react-helmet dependency and metadata config

--- src/eduverse_frontend/src/components/pages/home/Index.tsx
+++ src/eduverse_frontend/src/components/pages/home/Index.tsx
@@ -4,15 +4,29 @@ import Hero from './Hero';
 import { RefProvider } from '@/contexts/RefContext';
 import ValueProposition from './ValueProposition';
 import HowItWorks from './HowItWorks';
+import { Helmet } from 'react-helmet-async';
 
 export default function Home() {
   return (
-    <RootLayout header={<Navbar />}>
-      <RefProvider>
-        <Hero />
-        <ValueProposition />
-        <HowItWorks />
-      </RefProvider>
-    </RootLayout>
+    <>
+      <Helmet>
+        <title>EduVerse | Learn & Earn NFT Certificates on ICP</title>
+        <meta
+          name="description"
+          content="EduVerse is a decentralized education platform where you can learn, pass quizzes, and earn blockchain-verified NFT certificates powered by the Internet Computer (ICP)."
+        />
+        <meta
+          name="keywords"
+          content="EduVerse, decentralized education, blockchain learning, NFT certificates, ICP education platform, learn to earn"
+        />
+      </Helmet>
+      <RootLayout header={<Navbar />}>
+        <RefProvider>
+          <Hero />
+          <ValueProposition />
+          <HowItWorks />
+        </RefProvider>
+      </RootLayout>
+    </>
   );
 }
